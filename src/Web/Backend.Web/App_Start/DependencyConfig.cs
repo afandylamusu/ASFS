052f0f1359b9dfbf708330dfc08d6512a7809d7c
ADE-LENOVO320\Afandy <afandy.lamusu@yahoo.com>
2018-05-24 14:33:24 +0700

    Separate Dependency Register from Startup

---
+++ src/Web/Backend.Web/App_Start/DependencyConfig.cs
@@ -0,0 +1,47 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Backend.Web.Facades;
+using Backend.Web.Models.Forms;
+using FluentValidation;
+using System.Reflection;
+
+namespace Backend.Web
+{
+    internal class DependencyConfig
+    {
+        internal static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            RegisterServices(builder);
+
+            builder.RegisterControllers(typeof(Startup).Assembly).PropertiesAutowired();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
+
+            return builder.Build();
+        }
+
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacWebTypesModule());
+
+            builder
+                .RegisterAssemblyTypes(typeof(FeedbackFormValidator).GetTypeInfo().Assembly)
+                .Where(t => t.IsClosedTypeOf(typeof(IValidator<>)))
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<ProfileFacade>().As<IProfileFacade>().InstancePerLifetimeScope();
+            builder.RegisterType<AlertFacade>().As<IAlertFacade>().InstancePerLifetimeScope();
+            builder.RegisterType<FeedbackFacade>().As<IFeedbackFacade>().InstancePerLifetimeScope();
+            builder.RegisterType<ReportFacade>().As<IReportFacade>().InstancePerLifetimeScope();
+            builder.RegisterType<UserGroupAlertFacade>().As<IUserGroupAlertFacade>().InstancePerLifetimeScope();
+            builder.RegisterType<MasterFacade>().As<IMasterFacade>().InstancePerLifetimeScope();
+
+        }
+
+    }
+}
--- src/Web/Backend.Web/Startup.cs
+++ src/Web/Backend.Web/Startup.cs
@@ -1,12 +1,7 @@
-﻿using Autofac;
-using Autofac.Integration.Mvc;
+﻿using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
-using Backend.Web.Facades;
-using Backend.Web.Models.Forms;
-using FluentValidation;
 using Microsoft.Owin;
 using Owin;
-using System.Reflection;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -28,16 +23,6 @@ namespace Backend.Web
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
-            var builder = new ContainerBuilder();
-
-            RegisterServices(builder);
-
-            builder.RegisterControllers(typeof(Startup).Assembly).PropertiesAutowired();
-
-            // Register your Web API controllers.
-            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-            builder.RegisterAssemblyModules(Assembly.GetExecutingAssembly());
-
 
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
@@ -47,7 +32,7 @@ namespace Backend.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            var container = builder.Build();
+            var container = DependencyConfig.Build();
 
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
 
@@ -64,22 +49,5 @@ namespace Backend.Web
 
         }
 
-        private void RegisterServices(ContainerBuilder builder)
-        {
-            builder.RegisterModule(new AutofacWebTypesModule());
-
-            builder
-                .RegisterAssemblyTypes(typeof(FeedbackFormValidator).GetTypeInfo().Assembly)
-                .Where(t => t.IsClosedTypeOf(typeof(IValidator<>)))
-                .AsImplementedInterfaces();
-
-            builder.RegisterType<ProfileFacade>().As<IProfileFacade>().InstancePerLifetimeScope();
-            builder.RegisterType<AlertFacade>().As<IAlertFacade>().InstancePerLifetimeScope();
-            builder.RegisterType<FeedbackFacade>().As<IFeedbackFacade>().InstancePerLifetimeScope();
-            builder.RegisterType<ReportFacade>().As<IReportFacade>().InstancePerLifetimeScope();
-            builder.RegisterType<UserGroupAlertFacade>().As<IUserGroupAlertFacade>().InstancePerLifetimeScope();
-            builder.RegisterType<MasterFacade>().As<IMasterFacade>().InstancePerLifetimeScope();
-
-        }
     }
 }
